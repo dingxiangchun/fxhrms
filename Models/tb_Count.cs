adce9a34e7d21b6dc41a9add2bbb8693f3f8e9cd
dingxiangchun <454059034@qq.com>
2013-11-27 16:41:52 +0800

    统计

---
+++ Models/tb_Count.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class tb_Count
+    {
+        public tb_Count()
+        { }
+        #region Model
+        private string _CountName;
+        private Int64 _Count;
+
+        public string CountName
+        {
+            set { _CountName = value; }
+            get { return _CountName; }
+        }
+
+        public Int64 Count
+        {
+            set { _Count = value; }
+            get { return _Count; }
+        }
+        #endregion Model
+    }
+}
