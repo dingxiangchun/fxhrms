73e0c549610a4b96fc75ddd1776172cbcbf4ef1c
dingxiangchun <454059034@qq.com>
2013-11-27 21:41:13 +0800

    综合查询

--- perInfoQuery.aspx.cs
+++ perInfoQuery.aspx.cs
@@ -14,13 +14,60 @@ using Models;
 
 public partial class perInfoQuery : System.Web.UI.Page
 {
-    branch dal = new branch();
-    tb_branch model = new tb_branch();
+    perInfo dal = new perInfo();
+    tb_perInfo model = new tb_perInfo();
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        string strwhere = "1=1";
+        if (Request["branchid"] != null && Request["branchid"] != "0")
+        {
+            strwhere += " and UnitID=" + Request["branchid"];
+        }
+
+        if (Request["name"] != null&&Request["name"] != "")
+        {
+            strwhere += " and Name='" + Request["name"] + "'";
+        }
+
+        if (Request["Employeeid"] != null&&Request["Employeeid"] !="")
+        {
+            strwhere += " and Employeeid='" + Request["Employeeid"]+"'";
+        }
+
+        if (Request["age"] != null&&Request["age"] !="")
+        {
+            strwhere += " and (YEAR(CURDATE())-YEAR(STR_TO_DATE(tb_perinfo.Jobtime,\"%Y-%m-%d\"))) - (RIGHT(CURDATE(),5)<RIGHT(tb_perinfo.Jobtime,5))=" + Request["age"];
+        }
+
+        if (Request["Birth"] != null && Request["Birth"] != "")
+        {
+            strwhere += " and Birth='" + Request["Birth"] + "'";
+        }
+
+        if (Request["Rank"] != null && Request["Rank"] != "")
+        {
+            strwhere += " and Rank='" + Request["Rank"] + "'";
+        }
+
+        if (Request["Level"] != null && Request["Level"] != "")
+        {
+            strwhere += " and Level=" + Request["Rank"];
+        }
+
+        if (Request["fulltime_educ"] != null && Request["fulltime_educ"] != "")
+        {
+            strwhere += " and fulltime_educ='" + Request["fulltime_educ"]+"'";
+        }
+
+        if (Request["Guard"] != null && Request["Guard"] != "")
+        {
+            strwhere += " and Guard='" + Request["Guard"] + "'";
+        }
+
+        IList<tb_perInfo> list = dal.GetListAll(strwhere);
         VelocityHelper vh = new VelocityHelper();
         vh.Init();
+        vh.Put("list", list);
         vh.Display("query.vm");
     }
 }
